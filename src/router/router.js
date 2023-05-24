f9064394b2d2caacede36a86b61064860d4c5535
Brian <briangrji28@gmail.com>
2023-05-23 22:13:55 -0600

    arreglo rutas detalle anuncio

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DetalleAnuncio from "../components/DetalleAnuncio.vue";
-import InputIdAnuncio from "../components/InputIdAnuncio.vue";
 
 // Array de objetos con las rutas
 const routes = [
-  { path: "/", component: InputIdAnuncio },
   { path: "/detalle/:id", component: DetalleAnuncio },
 ];
 
